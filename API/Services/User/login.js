25e149e34fe4c42feeb765e3bc9c34153a20ff00
Y1 <youssefabdo4005@gmail.com>
2024-03-09 22:20:18 +0200

    --test

--- API/Services/User/login.js
+++ API/Services/User/login.js
@@ -1,31 +1,32 @@
 const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
 
 
 async function login(req, res) {
-    try {
-        const prisma = new PrismaClient();
-        const { email, password } = req.body;
-        const user = await prisma.user.findFirst({
-          where: { email },
-        });
-    
-        if (!user) {
-          return res.status(401).send('Invalid username or password');
-        }
-    
-        const isValid = await bcrypt.compare(password, user.password);
-    
-        if (!isValid) {
-          return res.status(401).send('Invalid username or password');
-        }
-    
-        const token = jwt.sign({ id: user.id }, 'secret');
-        res.send({ token , "id":user.id });
-      } catch (error) {
-        res.status(500).send('Error logging in');
-      }
+  try {
+    const bcrypt = require('bcrypt');
+    const jwt = require('jsonwebtoken');
+    const prisma = new PrismaClient();
+    const { email, password } = req.body;
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).send('Invalid username or password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      return res.status(401).send('Invalid username or password');
+    }
+
+    const token = jwt.sign({ id: user.id }, 'secret');
+    res.send({ token, "id": user.id });
+  } catch (error) {
+    res.status(500).send('Error logging in');
+  }
 }
 
 module.exports = login
