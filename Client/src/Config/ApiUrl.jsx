b7c4e947a3a1eaad0eb96ea39efd9bbe6c5847c0
Y1 <youssefabdo4005@gmail.com>
2024-03-16 20:50:49 +0200

    --server on

--- Client/src/Config/ApiUrl.jsx
+++ Client/src/Config/ApiUrl.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const ApiUrl = axios.create({
-    // baseURL: "https://win-it-server.vercel.app/",
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://win-it-server.vercel.app/",
+    // baseURL: "http://localhost:8000/",
     headers: {
         Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZThiMTYyZjQ1NTU1YjI3MTIzMGI2ZSIsImlhdCI6MTcxMDA1MTcxM30.10C2R0THEBUN4DrOTu6PA-UOY8sBgzUT31_gD0lVs9Q`,
     },
