bd75908e0ab8c6748f0007a7f9d9776f1b9196be
Y1 <youssefabdo4005@gmail.com>
2024-04-04 14:32:48 +0200

    --auth fix

--- API/Services/User/login.js
+++ API/Services/User/login.js
@@ -1,11 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
-
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 async function login(req, res) {
   try {
-    const bcrypt = require('bcrypt');
-    const jwt = require('jsonwebtoken');
     const prisma = new PrismaClient();
     const { email, password } = req.body;
     const user = await prisma.user.findFirst({
@@ -13,27 +11,28 @@ async function login(req, res) {
     });
 
     if (!user) {
-      return res.send({
-        error: 'Not found'
+      return res.status(404).json({
+        error: 'User not found'
       });
     }
 
     const isValid = await bcrypt.compare(password, user.password);
 
     if (!isValid) {
-      return res.json({
+      return res.status(401).json({
         error: 'Invalid username or password'
       });
     }
 
     const token = jwt.sign({ id: user.id }, 'secret');
-    res.send({ token, "id": user.id, user: user });
+    res.status(200).json({ token, id: user.id, user });
   } catch (error) {
-    res.status(500).send({
+    console.error('Error while logging in:', error);
+    res.status(500).json({
       error: 'An error occurred while logging in',
       message: error.message,
     });
   }
 }
 
-module.exports = login
+module.exports = login;
--- API/Services/User/register.js
+++ API/Services/User/register.js
@@ -1,46 +1,47 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
 const prisma = new PrismaClient();
 
 async function register(req, res) {
-    try {
-        const { username, email , password } = req.body;
-    
-        // Validate username and password
-        if (!username || !password) {
-          return res.status(400).send('Username and password are required');
-        }
-    
-        // Additional validation logic can be added here
-    
-        const existingUser = await prisma.user.findFirst({
-          where: { username },
-        });
-    
-        if (existingUser) {
-          return res.status(400).send({
-            error: 'Username already exists',
-          });
-        }
-    
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await prisma.user.create({
-          data: {
-            username,
-            email,
-            password: hashedPassword,
-          },
-        });
-        
-        const token = jwt.sign({ id: newUser.id }, 'secret');
-        res.json({ token });
-      } catch (error) {
-        console.log(error)
-        res.status(500).send({
-          error: 'An error occurred while registering',
-        });
-      }
+  try {
+    const { username, email, password } = req.body;
+
+    // Validate username, email, and password
+    if (!username || !email || !password) {
+      return res.status(400).send('Username, email, and password are required');
+    }
+
+    // Additional validation logic can be added here
+
+    const existingUser = await prisma.user.findFirst({
+      where: { OR: [{ username }, { email }] },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        error: 'Username or email already exists',
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const token = jwt.sign({ id: newUser.id }, 'secret');
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error('Error while registering:', error);
+    res.status(500).json({
+      error: 'An error occurred while registering',
+    });
+  }
 }
 
-module.exports = register
+module.exports = register;
